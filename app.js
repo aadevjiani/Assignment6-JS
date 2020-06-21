d391ebd08bd629a5f0ceff64c3aeed9ed5a5ffb7
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-06-21 11:54:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// Assignment by Ali Abbas Devjiani
+// Assignment No. 6 - Chapter 21 - 38
+// String Methods
+//Chapter 21-25 - Task No. 1
+// var firstName = prompt("Enter the First Name : ")
+// var secondName = prompt("Enter the Second Name : ")
+// var fullName = (firstName + " " + secondName)
+// alert("Hello Mr./Ms " + fullName + " Welcome to Java World")
+
+//Chapter 21-25 - Task No. 2
+// var favMobileModel = prompt("Enter the name of your Favourite Mobile Phone Model : ")
+// var lenFavMobileModel = favMobileModel.length
+// alert("My favourite phone is : " + favMobileModel + "\n" + "Length of String : " + lenFavMobileModel)
+
+//Chapter 21-25 - Task No. 3
+var string = "Pakistani"
+document.write("String : " + string)
+document.write("<br/>")
+document.write("Index of 'n' : " + string.indexOf("n"))
