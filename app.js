3367280806a95a7aab1bf17d436a3e5fae99c6c9
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-06-21 19:24:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -13,7 +13,40 @@
 // alert("My favourite phone is : " + favMobileModel + "\n" + "Length of String : " + lenFavMobileModel)
 
 //Chapter 21-25 - Task No. 3
-var string = "Pakistani"
-document.write("String : " + string)
+// var string = "Pakistani"
+// document.write("String : " + string)
+// document.write("<br/>")
+// document.write("Index of 'n' : " + string.indexOf("n"))
+
+//Chapter 21-25 - Task No. 4
+// var string = "Hello World"
+// document.write("String : " + string)
+// document.write("<br/>")
+// document.write("Last index of 'l' : " + string.lastIndexOf("l"))
+
+//Chapter 21-25 - Task No. 5
+// var string = "Pakistani"
+// document.write("String : " + string)
+// document.write("<br/>")
+// document.write("Character at Index 3 : " + string.charAt(3))
+
+//Chapter 21-25 - Task No. 6
+// var firstName = prompt("Enter the First Name : ")
+// var secondName = prompt("Enter the Second Name : ")
+// var fullName = firstName.concat(" " + secondName)
+// alert("Hello Mr./Ms " + fullName + " Welcome to Java World")
+
+//Chapter 21-25 - Task No. 7
+// var city = "Hyderabad"
+// document.write("City : " + city)
+// document.write("<br/>")
+// updatedCity = city.replace("Hyder","Islam")
+// document.write("After replacement : " + updatedCity)
+
+//Chapter 21-25 - Task No. 8
+var message = "Ali and Sami are best friends. They play cricket and football together."
+document.write(message)
 document.write("<br/>")
-document.write("Index of 'n' : " + string.indexOf("n"))
+updatedMessage = message.replace("and","&")
+document.write(updatedMessage)
+
